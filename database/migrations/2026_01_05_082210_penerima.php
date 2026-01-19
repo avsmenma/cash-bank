19978306c072e039d8416f2addc1d1b6af0c1f51
arcadealley <ketsutol1@gmail.com>
2026-01-20 01:15:55 +0700

    846

--- database/migrations/2026_01_05_082210_penerima.php
+++ database/migrations/2026_01_05_082210_penerima.php
@@ -4,24 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('penerimas', function(Blueprint $table){
+        Schema::create('penerimas', function (Blueprint $table) {
             $table->id('id_penerima');
-            $table->String('kontrak');
-            $table->String('pembeli');
-            $table->date('tanggal');
-            $table->String('no_reg');
-            $table->decimal('volume', 38,2)->default(0);
-            $table->decimal('harga', 38,2)->default(0);
-            $table->decimal('nilai', 38,2)->default(0);
-            $table->decimal('ppn', 38,2)->default(0);
-            $table->decimal('potPPH', 38,2)->default(0);
+            $table->string('kontrak')->nullable();
+            $table->unsignedBigInteger('id_kategori_kriteria')->nullable();
+            $table->string('pembeli')->nullable();
+            $table->date('tanggal')->nullable();
+            $table->string('no_reg')->nullable();
+            $table->decimal('volume', 38, 2)->default(0);
+            $table->decimal('harga', 38, 2)->default(0);
+            $table->decimal('nilai', 38, 2)->default(0);
+            $table->decimal('ppn', 38, 2)->default(0);
+            $table->decimal('potppn', 38, 2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('id_kategori_kriteria')
+                ->references('id_kategori_kriteria')
+                ->on('kategori_kriteria')
+                ->nullOnDelete();
         });
     }
 
@@ -30,6 +36,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('penerimas');
     }
 };

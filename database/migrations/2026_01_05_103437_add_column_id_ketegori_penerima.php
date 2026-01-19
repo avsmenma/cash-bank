b2e7b89b20b63785fa44a84f0fe6744ff68ae562
arcadealley <ketsutol1@gmail.com>
2026-01-20 01:18:43 +0700

    8469

--- database/migrations/2026_01_05_103437_add_column_id_ketegori_penerima.php
+++ database/migrations/2026_01_05_103437_add_column_id_ketegori_penerima.php
@@ -4,27 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::table('penerimas', function(Blueprint $table){
-            $table->unsignedBigInteger('id_kategori_kriteria')->nullable()->after('id_penerima');
-            $table->foreign('id_kategori_kriteria')
-            ->references('id_kategori_kriteria')
-            ->on('kategori_kriteria')
-            ->nullOnDelete();
-        });
+        // Column and FK already added in create table migration
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        // No action needed
     }
 };

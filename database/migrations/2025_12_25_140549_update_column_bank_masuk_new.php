f4ce5c7bfcc6e1d303e4e523f4942b031c416e0e
arcadealley <ketsutol1@gmail.com>
2026-01-20 01:02:16 +0700

    843975

--- database/migrations/2025_12_25_140549_update_column_bank_masuk_new.php
+++ database/migrations/2025_12_25_140549_update_column_bank_masuk_new.php
@@ -4,32 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-             Schema::table('bank_masuk', function(Blueprint $table){
-            $table->unsignedBigInteger('id_sub_kriteria')->nullable()->after('id_kategori_kriteria');
-            $table->foreign('id_sub_kriteria')
-            ->references('id_sub_kriteria')
-            ->on('sub_kriteria')
-            ->nullOnDelete();
-            $table->unsignedBigInteger('id_item_sub_kriteria')->nullable()->after('id_sub_kriteria');
-            $table->foreign('id_item_sub_kriteria')
-            ->references('id_item_sub_kriteria')
-            ->on('item_sub_kriteria')
-            ->nullOnDelete();
-        });
+        // Columns already added in 2025_12_25_112418
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        // No action needed
     }
 };

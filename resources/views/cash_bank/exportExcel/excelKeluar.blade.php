f8b0d45e059c8e79529565ebeda86f4c8840a54c
arcadealley <ketsutol1@gmail.com>
2026-01-29 14:30:55 +0700

    346793

--- resources/views/cash_bank/exportExcel/excelKeluar.blade.php
+++ resources/views/cash_bank/exportExcel/excelKeluar.blade.php
@@ -1,94 +1,38 @@
-@push('styles')
-<style>
-    #example3 {
-    table-layout: auto !important;
-    width: 100% !important;
-    }
-
-    #example3 th,
-    #example3 td {
-        white-space: nowrap;        /* biar kolom melebar */
-        vertical-align: middle;
-    }
-</style>
-@endpush
-        <table id="example3" class="table table-bordered table-hover">
-            <thead>
-                <tr>
-                    <th><input type="checkbox" id="select_all_ids"></th>
-                    <th>No</th>
-                    <th>Agenda</th>
-                    <th>No Bukti</th>
-                    <th>Tanggal</th>
-                    <th>Sumber Dana</th>
-                    <th>Bank Tujuan</th>
-                    <th>Kriteria</th>
-                    <th>Sub Kriteria </th>
-                    <th>Item Sub Kriteria</th>
-                    <th>Penerima</th>
-                    <th>Uraian</th>
-                    <th>Jenis Pembayaran</th>
-                    <th>Kredit</th>
-                    <th>Keterangan</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-        </table>
-
-@push('scripts')
-<script>
-$(document).ready(function () {
-    $('#example3').DataTable({
-        processing: true,
-        serverSide: true,
-        ordering: false,
-        autoWidth: false,
-        ajax: "{{ route('bank-keluar.data') }}",
-        columns: [
-            {data: 'checkbox'},
-           {
-            data: 'DT_RowIndex',
-            orderable: false,
-            searchable: false,
-            title: 'No'
-        },
-            {data: 'agenda_tahun'},
-            {
-                data: 'DT_RowIndex',
-                orderable: false,
-                searchable: false,
-                title: 'No Bukti'
-            },
-            {data: 'tanggal'},
-            { data: 'sumber_dana' },
-            { data: 'bank_tujuan' },
-            { data: 'kategori_kriteria' },
-            { data: 'sub_kriteria' },
-            { data: 'item_sub_kriteria' },
-           
-            {data: 'penerima'},
-            {data: 'uraian',width: '50px'},
-            {data: 'jenis_pembayaran' },
-            {
-                data: 'kredit',
-                className: 'text-right',
-                render: function (data) {
-                    if (!data || data == 0) {
-                        return '0';
-                    }
-
-                    return Number(data).toLocaleString('id-ID', {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2
-                    });
-                }
-            },
-            {data: 'keterangan'},
-            {data: 'aksi', orderable:false, searchable:false}
-        ]
-    });
-});
-</script>
-@endpush
-@include('cash_bank.modal.editKeluar')
-
+<table>
+    <thead>
+        <tr>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">No</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Agenda</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Tanggal</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Sumber Dana</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Bank Tujuan</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Kriteria</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Sub Kriteria</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Item Sub Kriteria</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Penerima</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Uraian</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Jenis Pembayaran</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Kredit</th>
+            <th style="background-color: #dc3545; color: white; font-weight: bold;">Keterangan</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($data as $index => $item)
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $item->agenda_tahun }}</td>
+                <td>{{ $item->tanggal }}</td>
+                <td>{{ $item->sumberDana->nama_sumber_dana ?? '-' }}</td>
+                <td>{{ $item->bankTujuan->nama_tujuan ?? '-' }}</td>
+                <td>{{ $item->kategori->nama_kriteria ?? '-' }}</td>
+                <td>{{ $item->subKriteria->nama_sub ?? '-' }}</td>
+                <td>{{ $item->itemSubKriteria->nama_item ?? '-' }}</td>
+                <td>{{ $item->penerima }}</td>
+                <td>{{ $item->uraian }}</td>
+                <td>{{ $item->jenisPembayaran->nama_jenis_pembayaran ?? '-' }}</td>
+                <td style="text-align: right;">{{ number_format($item->kredit, 2, ',', '.') }}</td>
+                <td>{{ $item->keterangan }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>

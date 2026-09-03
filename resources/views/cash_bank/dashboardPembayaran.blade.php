18ae5a697c5f8bf291920e65f7cfc12659d35793
arcadealley <ketsutol1@gmail.com>
2026-09-03 09:54:51 +0700

    style(dashboard-pembayaran): hapus label content-header dan breadcrumbs pada submenu PD & PvD

--- resources/views/cash_bank/dashboardPembayaran.blade.php
+++ resources/views/cash_bank/dashboardPembayaran.blade.php
@@ -1,21 +1,6 @@
 @extends('layouts/index')
 @section('content')
-<div class="container-fluid m-3">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Dashboard PD & PvD</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active"><a href="{{ route('dashboard.index') }}">Dashboard Pembayaran</a></li>
-            </ol>
-          </div>
-        </div>
-      </div>
-    </section>
+<div class="container-fluid pt-3 px-3">
     <section class="content">
       <div class="row">
         <div class="col-md-12">

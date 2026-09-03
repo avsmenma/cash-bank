28ed1b531596f53b657f632365f7075b267dde28
arcadealley <ketsutol1@gmail.com>
2026-09-03 09:54:54 +0700

    style(modal-kerja): hapus label content-header dan breadcrumbs pada submenu Modal Kerja

--- resources/views/cash_bank/modalKerja.blade.php
+++ resources/views/cash_bank/modalKerja.blade.php
@@ -1,21 +1,7 @@
 @extends('layouts.index')
 
 @section('content')
-<div class="container-fluid m-3">
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6"><h1>Modal Kerja</h1></div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Modal Kerja</li>
-                    </ol>
-                </div>
-            </div>
-        </div>
-    </section>
-
+<div class="container-fluid pt-3 px-3">
     <section class="content">
         <div class="row">
             <div class="col-md-12">
